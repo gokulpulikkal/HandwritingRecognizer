a048086cad9841bdbe0de0a5c88961c35a234685
Gokul <gokulplkl@gmail.com>
2023-11-16 00:09:53 -0500

    Initial Commit

---
+++ MNISTRecogniser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MNISTRecogniser
+//
+//  Created by Gokul P on 16/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
